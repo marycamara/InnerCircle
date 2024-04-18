cabae07252ac522f30a2f5808a6128d08b4127be
marycamara <camaram3@coventry.ac.uk>
2024-04-18 03:41:48 +0100

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace InnerCircle;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace InnerCircle;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
